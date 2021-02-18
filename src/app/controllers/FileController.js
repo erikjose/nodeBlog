388e6794f296080e31a23f26a77175739472cdbc
Erik José <serikjose@gmail.com>
2021-02-18 07:04:00 -0300

    sign commits two

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -4,10 +4,7 @@ class FileController {
   async store(req, res) {
     const { filename: path, originalname: name } = req.file;
 
-    const file = await File.create({
-      name,
-      path,
-    });
+    const file = await File.create({ name, path });
 
     return res.json(file);
   }
